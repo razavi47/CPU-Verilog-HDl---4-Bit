ad10e849bb5110d30b285c7999a070b22b7f2586
razavi47 <nimarazavi05@gmail.com>
2025-11-22 12:38:25 -0500

    instruction memory testbench structure

--- src/instruction_memory_tb.v
+++ src/instruction_memory_tb.v
@@ -1,59 +1,34 @@
 `timescale 1ns/1ps
 
-module counter_tb;
-
+module instruction_memory_tb;
     reg clk;
-    reg reset;
-    reg jump_en;
-    reg [3:0] jump_addr;
-    reg halt;
-    reg ir_load_en;
-    wire [3:0] pc_out;
+    reg [3:0] pc_addr;
+    wire [7:0] instruction_out;
 
-    counter dut (
+    instruction_memory dut (
         .clk(clk),
-        .reset(reset),
-        .jump_en(jump_en),
-        .jump_addr(jump_addr),
-        .halt(halt),
-        .ir_load_en(ir_load_en),
-        .pc_out(pc_out)
+        .pc_addr(pc_addr),
+        .instruction_out(instruction_out)
     );
 
     always #5 clk = ~clk;
 
     initial begin
         clk = 0;
-        reset = 0;
-        jump_en = 0;
-        jump_addr = 4'b0000;
-        halt = 0;
-        ir_load_en = 0;
+        pc_addr = 0;
 
-        // Apply reset
-        #10 reset = 1;
-        #10 $display("RESET RELEASED\n");
-
-        #50;
-        jump_addr = 4'b1010;   
-        jump_en   = 1;
-        #10;
-        jump_en   = 0;
-        #20;
-
-        halt = 1;
-        #40;
-        halt = 0;
-
-        ir_load_en = 1;
-        #40;
-
-        $finish;
+        #10 pc_addr = 4'd0;   // expect 0101_0010
+        #10 pc_addr = 4'd1;   // expect 0101_0111
+        #10 pc_addr = 4'd2;   // expect 1000_0001
+        #10 pc_addr = 4'd3;   // expect 1111_0000
+        #10 pc_addr = 4'd4;   // expect 0000_0000
+        #10 pc_addr = 4'd5;   // expect 0000_0000
+        #20 $finish;
     end
 
     initial begin
-        $monitor("t=%0t | pc_out=%b | reset=%b jump=%b addr=%b halt=%b ir_load=%b",
-                 $time, pc_out, reset, jump_en, jump_addr, halt, ir_load_en);
+        $monitor("t=%0t | PC=%d | instruction=%b",
+                  $time, pc_addr, instruction_out);
     end
 
 endmodule
